10bf685b560efb885f25c8b337fb9026349e75d5
emre-a <77207279+emre-a@users.noreply.github.com>
2022-10-19 10:53:45 +0300

    Update main.c
    second commit

--- main.c
+++ main.c
@@ -3,7 +3,8 @@ this is first commit to understand the commit history
 #define <stdio.h>
 
 void main(){
-  int i = 2;
+  int i = 3;
+  char array[]="second commit";
   
   printf("Nothing to do!");
 }
